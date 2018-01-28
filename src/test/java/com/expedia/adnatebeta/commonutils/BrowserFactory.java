5e5257010a30f6f34b864171bc1c0ab3a4161705
adityabharadwaj <adityabharadwaj@192.168.2.3>
2018-01-28 18:36:38 +0530

    pushing changes 1!

--- src/test/java/com/expedia/adnatebeta/commonutils/BrowserFactory.java
+++ src/test/java/com/expedia/adnatebeta/commonutils/BrowserFactory.java
@@ -4,14 +4,12 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Platform;
-import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 
